df73793f78ff77eea85225b88a5b09c58f048f38
Pedro Furlan <pedrofurlan6@gmail.com>
2025-04-08 21:57:51 -0300

    feat: creating edit customer use case

---
+++ backend/src/domain/rental/application/use-cases/edit-customer-use-case.ts
@@ -0,0 +1,53 @@
+import { Customer } from '../../enterprise/entities/customer';
+import { Injectable } from '@nestjs/common';
+import { CustomerRepository } from '../repositories/customer-repository';
+import { Either, left, right } from '../../../../core/either';
+import { HashGenerator } from '../cryptography/hash-generator';
+import { CustomerNotFoundError } from './errors/customer-not-found-error';
+
+interface EditCustomerUseCaseRequest {
+  customerId: string;
+  name: string;
+  phone: string;
+  password?: string | null;
+}
+
+type EditCustomerUseCaseResponse = Either<
+  CustomerNotFoundError,
+  { customer: Customer }
+>;
+
+@Injectable()
+export class EditCustomerUseCase {
+  constructor(
+    private customerRepository: CustomerRepository,
+    private readonly hashGenerator: HashGenerator,
+  ) {}
+
+  async execute({
+    customerId,
+    name,
+    phone,
+    password,
+  }: EditCustomerUseCaseRequest): Promise<EditCustomerUseCaseResponse> {
+    const customerSelected = await this.customerRepository.findById(customerId);
+
+    if (!customerSelected) {
+      return left(new CustomerNotFoundError());
+    }
+
+    customerSelected.name = name;
+    customerSelected.phone = phone;
+
+    if (password) {
+      const hashedPassword = await this.hashGenerator.hash(password);
+      customerSelected.password = hashedPassword;
+    }
+
+    await this.customerRepository.update(customerSelected);
+
+    return right({
+      customer: customerSelected,
+    });
+  }
+}
