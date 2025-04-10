872cffe2697108e6d828d641c5b55e6944bb26d9
Pedro Furlan <pedrofurlan6@gmail.com>
2025-04-10 10:31:01 -0300

    feat: creating delete booking use case

---
+++ backend/src/domain/rental/application/use-cases/delete-booking-use-case/delete-booking-use-case.ts
@@ -0,0 +1,36 @@
+import { Either, left, right } from '../../../../../core/either';
+import { BookingRepository } from '../../repositories/booking-repository';
+import { BookingNotFoundError } from '../errors/booking-not-found-error';
+import { BookingDoesNotBelongToCustomerError } from '../errors/booking-does-not-belong-to-customer-error';
+
+interface DeleteBookingUseCaseRequest {
+  bookingId: string;
+  customerId: string;
+}
+
+type DeleteBookingUseCaseResponse = Either<
+  BookingNotFoundError | BookingDoesNotBelongToCustomerError,
+  void
+>;
+
+export class DeleteBookingUseCase {
+  constructor(private bookingRepository: BookingRepository) {}
+
+  async execute({
+    bookingId,
+    customerId,
+  }: DeleteBookingUseCaseRequest): Promise<DeleteBookingUseCaseResponse> {
+    const booking = await this.bookingRepository.findById(bookingId);
+
+    if (!booking) {
+      return left(new BookingNotFoundError());
+    }
+
+    if (booking.customerId.toString() !== customerId) {
+      return left(new BookingDoesNotBelongToCustomerError());
+    }
+
+    await this.bookingRepository.delete(bookingId);
+    return right(undefined);
+  }
+}
